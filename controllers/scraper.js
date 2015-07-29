0ba9c6f9b35628bfa7c275325d73edf6b8fda5cd
Manh Thiep <manhthiep@gmail.com>
2015-07-30 00:21:24 +0700

    Remove scrape jobs Clean up unused files

--- controllers/scraper.js
+++ controllers/scraper.js
@@ -7,65 +7,30 @@ var ejs = require('ejs');
 
 var configPath = require("../config/path");
 
-var Jobs = require('./jobs');
 var Projects = require('./projects');
 
-// GET /scraper?link=...
+// GET /
 exports.showScraperPage = function(req, res) {
   console.log('Show scraper page');
-  if (typeof req.query.link == 'undefined') {
-    // return res.render('scraper-view.ejs', {
-    //   user: req.user,
-    //   page: {},
-    //   job: {},
-    //   urlutil: urlutil
-    // });
-    // return res.render('scraper-main.ejs', {
-    //   user: req.user
-    // });
-    Projects.getProjectsInternal(req.user, {}, function(err, result) {
-      if (err) {
-        console.log(err);
-        return res.render('scraper-main.ejs', {
-          user: req.user,
-          top_projects: []
-        });
-      }
-      // Sort projects by last update
-      result.projects.sort(function(a, b){
-        var dateA = new Date(a.last_update),
-        dateB = new Date(b.last_update);
-        return dateB-dateA;
-      });
+  Projects.getProjectsInternal(req.user, {}, function(err, result) {
+    if (err) {
+      console.log(err);
       return res.render('scraper-main.ejs', {
         user: req.user,
-        top_projects: result.projects
+        top_projects: []
       });
+    }
+    // Sort projects by last update
+    result.projects.sort(function(a, b){
+      var dateA = new Date(a.last_update),
+      dateB = new Date(b.last_update);
+      return dateB-dateA;
     });
-  } else {
-    scrapeInternal({ 
-      url: req.query.link,
-      override_url: '/scraper?link='
-    }, function(err, result) {
-      if(err){
-        console.log(err);
-        return res.render('scraper-view.ejs', {
-          error: err,
-          user: req.user,
-          page: (result) ? result.page : { url: req.query.link },
-          job: (result) ? result.job : {},
-          urlutil: urlutil
-        });
-      }
-      return res.render('scraper-view.ejs', {
-        error: null,
-        user: req.user,
-        page: result.page,
-        job: result.job,
-        urlutil: urlutil
-      });
+    return res.render('scraper-main.ejs', {
+      user: req.user,
+      top_projects: result.projects
     });
-  }
+  });
 }
 
 function requestWithEncoding(options, callback) {
@@ -108,251 +73,14 @@ function readScriptFileSync(script_file) {
   return fs.readFileSync(configPath.rootPath + '/views/websites/' + script_file, 'utf-8');
 }
 
-// options: 
-// {
-//   url: String,
-//   no_default: String, ('yes' or 'no')
-//   override_url: String ('http://.../scraper?link=')
-// }
-function scrapeInternal(options, callback) {
-  console.log('Scrape: options.url=' + options.url);
-  requestWithEncoding(
-    {
-      url: options.url,
-      headers: {
-        'User-Agent': 'request'
-      },
-      timeout: 20000 /* 20 seconds */
-    },
-    function(error, response, html){
-      if(error){
-        console.log(error);
-        return callback(error);
-      }
-      
-      var page = {
-        url: '',
-        title: '',
-        description: '',
-        image: '',
-        html: '',
-        css: '',
-        links: []
-      };
-
-      page.url = response.request.href;
-      console.log("URL: " + page.url);
-
-      var content_type = response.headers['content-type'];
-      if (content_type && content_type.indexOf('html') == -1) {
-        console.log(response.headers);
-        return callback(new Error('Not HTML page (' + content_type + ')'));
-      }
-
-      console.log(response.headers);
-      console.log("HTML length: " + html.length);
-
-      var $ = cheerio.load(html);
-
-      page.title = $('title').first().text();
-      page.description = $('meta[name*=description]').attr('content');
-      if (page.title) {
-        page.title = page.title.replace(/(\r\n|\n|\r)/gm, '');
-      }
-      if (page.description) {
-        page.description = page.description.replace(/(\r\n|\n|\r)/gm, '');
-      }
-
-      // Open Graph meta tags
-      var og_metadata = {
-        url: $('meta[property="og:url"]').attr('content'),
-        type: $('meta[property="og:type"]').attr('content'),
-        title: $('meta[property="og:title"]').attr('content'),
-        description: $('meta[property="og:description"]').attr('content'),
-        image: $('meta[property="og:image"]').attr('content')
-      };
-      if (og_metadata.image) {
-        page.image = og_metadata.image;
-      }
-      if (page.description == '' && og_metadata.description && og_metadata.description != '') {
-        page.description = og_metadata.description.replace(/(\r\n|\n|\r)/gm, '');
-      }
-
-      console.log('Page title: ' + page.title);
-      console.log('Page description: ' + page.description);
-  
-      var job = {
-        id: '',
-        url_matches: [],
-        script: '',
-        template: '',
-        style: ''
-      };
-
-      Jobs.matchJobInternal(page.url, function(err, jobs) {
-        if (err) {
-          console.log(err);
-          return callback(new Error('Find matching job failed'));
-        }
-
-        var baseTemplateFile = configPath.rootPath + "/views/scraper-base.ejs";
-
-        fs.readFile(baseTemplateFile, 'utf8', function (err, template) {
-          if (err) {
-            console.log(err);
-            return callback(new Error('Reading template file failed'));
-          }
-
-          if (jobs && jobs.length > 0) {
-            console.log('Matched jobs: ' + jobs.length);
-
-            job.id = jobs[0]._id;
-            job.url_matches = jobs[0].url_matches;
-            job.script = jobs[0].script;
-            job.template = jobs[0].template;
-            job.style = jobs[0].style;
-
-            // console.log('Matched Job [0]: ');
-            // console.log(job);
-
-          } else if (!options.no_default || options.no_default !== 'yes') {
-            job.script = '' +
-              'if ($(\'article\').length) {\n' +
-              '  $(\'article script\').remove();\n' +
-              '  page.content = $(\'article\').html();\n' +
-              '} else {\n' +
-              '  $(\'body script\').remove();\n' +
-              '  page.content = $(\'body\').html();\n' +
-              '}'
-            ;
-            job.template = '<%-page.content%>';
-          }
-
-          page.css = job.style;
-
-          // append job script & template to template
-          if (job.script != '') job.script = job.script.replace(/(<%|%>)/gm, '');
-          template += '<%' + job.script + '%>';
-          if (job.template == '') {
-            job.template = '<%-page.html%>';
-          }
-          template += job.template;
-
-          // console.log('Template:');
-          // console.log(template);
-
-          var resultHtml = '';
-          try {
-            resultHtml = ejs.render(template, {
-              page: page,
-              job: job,
-              $: $,
-              cheerio: cheerio,
-              urlutil: urlutil,
-              readScriptFileSync: readScriptFileSync,
-              override_url: options.override_url
-            });
-          } catch (e) {
-            console.log(e);
-            return callback(e, {
-              request_url: options.url,
-              page: page,
-              job: job
-            });
-          }
-
-          // console.log('Result (HTML):');
-          // console.log(resultHtml);
-
-          if (page.html == '') {
-            page.html = resultHtml;
-          }
-
-          return callback(null, {
-            request_url: options.url,
-            page: page,
-            job: job
-          });
-        });
-      });
-    }
-  );
-}
-
-// GET /scraper2?link=...
-exports.showScraper2Page = function(req, res) {
-  console.log('Show scraper2 page');
-  if (typeof req.query.link == 'undefined') {
-    return res.render('scraper-view2.ejs', {
-      user: req.user,
-      link_url: '',
-      link_html: '',
-      cheerio: cheerio,
-      urlutil: urlutil,
-      readScriptFileSync: readScriptFileSync
-    });
-  }
-  requestWithEncoding(
-    {
-      url: req.query.link,
-      headers: {
-        'User-Agent': 'request'
-      },
-      timeout: 20000 /* 20 seconds */
-    },
-    function(error, response, html){
-      if(error){
-        console.log(error);
-        return res.render('scraper-view2.ejs', {
-          user: req.user,
-          link_url: req.query.link,
-          link_html: null,
-          cheerio: cheerio,
-          urlutil: urlutil,
-          readScriptFileSync: readScriptFileSync
-        });
-      }
-      
-      var link_url = response.request.href;
-      console.log("URL: " + link_url);
-
-      var content_type = response.headers['content-type'];
-      if (content_type && content_type.indexOf('html') == -1) {
-        console.log(response.headers);
-        console.log('Not HTML page (' + content_type + ')'); 
-        return res.render('scraper-view2.ejs', {
-          user: req.user,
-          link_url: link_url,
-          link_html: '',
-          cheerio: cheerio,
-          urlutil: urlutil,
-          readScriptFileSync: readScriptFileSync
-        });
-      }
-
-      console.log(response.headers);
-      console.log("HTML length: " + html.length);
-
-      return res.render('scraper-view2.ejs', {
-        user: req.user,
-        link_url: link_url,
-        link_html: html,
-        cheerio: cheerio,
-        urlutil: urlutil,
-        readScriptFileSync: readScriptFileSync
-      });
-    }
-  );
-}
-
 // Scrape a link for information
-// GET /scrape?link=...
 // GET /scrape?project=...&link=...
 // optional:
 //  + no_default=... ('yes' or 'no')
-//  + override_url=... (example: 'http://scraper.jul11.co/scraper?link=')
+//  + override_url=... (example: 'http://scraper.jul11.co/projects/...?link=')
 exports.scrape = function(req, res) {
-  if (!req.query.link || req.query.link == "") {
+  if (!req.query.link || req.query.link == "" 
+    || !req.query.project || req.query.project == "") {
     return res.json({
       error: {
         type: "MissingArgumentError",
@@ -365,20 +93,13 @@ exports.scrape = function(req, res) {
     no_default: req.query.no_default,
     override_url: req.query.override_url
   };
-  if (req.query.project && req.query.project != '') {
-    Projects.getProjectById(req.query.project, function(err, project) {
-      if (err) return callback(err);    
-      return runScrapeProject(project, scrape_options, function(err, result) {
-        if (err) return res.json({ error: err.message });
-        res.json(result);
-      });
-    });
-  } else {
-    return scrapeInternal(scrape_options, function(err, result) {
-      if (err) return res.json({ error: err });
+  Projects.getProjectById(req.query.project, function(err, project) {
+    if (err) return callback(err);    
+    return runScrapeProject(project, scrape_options, function(err, result) {
+      if (err) return res.json({ error: err.message });
       res.json(result);
     });
-  }
+  });
 }
 
 // options: 
--- routes.js
+++ routes.js
@@ -1,6 +1,5 @@
 
 var Scraper = require('./controllers/scraper');
-var Jobs = require('./controllers/jobs');
 var Projects = require('./controllers/projects');
 
 module.exports = function(app) {
@@ -15,18 +14,8 @@ module.exports = function(app) {
 
   // SCRAPER
 
-  app.get('/scraper', Scraper.showScraperPage);
-  // app.get('/scraper2', Scraper.showScraper2Page);
   app.get('/scrape', Scraper.scrape);
   app.post('/scrape/test', Scraper.scrapeTest);
-
-  // JOBS
-
-  app.get('/jobs', Jobs.getJobs);
-  app.post('/jobs/add', Jobs.addJob);
-  app.post('/jobs/update', Jobs.updateJob);
-  app.get('/jobs/:job_id', Jobs.getJobInfo);
-  app.get('/jobs/:job_id/info', Jobs.getJobInfo);
 
   // PROJECTS
 
